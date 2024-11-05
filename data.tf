cdd7474a8b71049007222da79b6ac6cc2caa2833
ljinsu95 <ljinsu@insideinfo.co.kr>
2024-11-05 16:01:51 +0900

    tfe provider 삭제 및 참조하지 않는 data source 삭제

--- data.tf
+++ data.tf
@@ -8,21 +8,21 @@ data "aws_vpc" "selected" {
 }
 
 ### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/security_groups
-data "aws_security_groups" "main" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.selected.id]
-  }
-}
+# data "aws_security_groups" "main" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.selected.id]
+#   }
+# }
 
 ## 인터넷 게이트웨이 조회
 ### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/internet_gateway
-data "aws_internet_gateway" "default" {
-  filter {
-    name   = "attachment.vpc-id"
-    values = [data.aws_vpc.selected.id]
-  }
-}
+# data "aws_internet_gateway" "default" {
+#   filter {
+#     name   = "attachment.vpc-id"
+#     values = [data.aws_vpc.selected.id]
+#   }
+# }
 
 ## 서브넷 리스트 조회
 data "aws_subnets" "main" {
@@ -36,7 +36,7 @@ data "aws_subnets" "main" {
   }
 }
 
-data "aws_availability_zones" "available" {}
+# data "aws_availability_zones" "available" {}
 
 # 아마존 리눅스 2 이미지 조회
 ### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ami
--- provider.tf
+++ provider.tf
@@ -1,9 +1,3 @@
-provider "tfe" {
-  hostname     = "jinsu.terraform.insideinfo-cloud.com"
-  organization = var.TFE_ORG_NAME
-  token        = var.TFE_TOKEN
-}
-
 provider "aws" {
   region     = var.aws_region
   access_key = var.AWS_ACCESS_KEY_ID
