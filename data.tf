223bf385d075b8f67de0af0fbb51b49e794e6cb4
ljinsu95 <ljinsu@insideinfo.co.kr>
2025-01-14 15:09:43 +0900

    코드 정리

--- data.tf
+++ data.tf
@@ -7,23 +7,6 @@ data "aws_vpc" "selected" {
   id = data.aws_vpcs.existing.ids[0]
 }
 
-### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/security_groups
-# data "aws_security_groups" "main" {
-#   filter {
-#     name   = "vpc-id"
-#     values = [data.aws_vpc.selected.id]
-#   }
-# }
-
-## 인터넷 게이트웨이 조회
-### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/internet_gateway
-# data "aws_internet_gateway" "default" {
-#   filter {
-#     name   = "attachment.vpc-id"
-#     values = [data.aws_vpc.selected.id]
-#   }
-# }
-
 ## 서브넷 리스트 조회
 data "aws_subnets" "main" {
   filter {
